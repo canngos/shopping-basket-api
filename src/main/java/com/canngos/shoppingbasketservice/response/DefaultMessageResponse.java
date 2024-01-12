5ac562fd3743399546ae6326496b101bb50843d6
Can Baturlar <baturlarcan@gmail.com>
2024-01-13 02:43:07 +0300

    Message response created

---
+++ src/main/java/com/canngos/shoppingbasketservice/response/DefaultMessageResponse.java
@@ -0,0 +1,12 @@
+package com.canngos.shoppingbasketservice.response;
+
+import com.canngos.shoppingbasketservice.response.base.BaseResponse;
+import com.canngos.shoppingbasketservice.response.body.DefaultMessageBody;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class DefaultMessageResponse extends BaseResponse<DefaultMessageBody> {}
---
+++ src/main/java/com/canngos/shoppingbasketservice/response/body/DefaultMessageBody.java
@@ -0,0 +1,12 @@
+package com.canngos.shoppingbasketservice.response.body;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class DefaultMessageBody {
+    private String message;
+}
