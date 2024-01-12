68a660dab2378f4f55b4648d8d77af53b2b0a697
Can Baturlar <baturlarcan@gmail.com>
2024-01-13 02:21:02 +0300

    Customer entity created

---
+++ src/main/java/com/canngos/shoppingbasketservice/entity/Customer.java
@@ -0,0 +1,33 @@
+package com.canngos.shoppingbasketservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String address;
+    private String password;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
