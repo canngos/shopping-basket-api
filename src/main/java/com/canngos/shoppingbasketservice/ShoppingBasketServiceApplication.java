929407ce2061407a2753ce3586f3076711e6b0a4
Can Baturlar <baturlarcan@gmail.com>
2024-01-12 23:50:22 +0300

    Project initialized

---
+++ src/main/java/com/canngos/shoppingbasketservice/ShoppingBasketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.canngos.shoppingbasketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBasketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingBasketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canngos/shoppingbasketservice/ShoppingBasketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canngos.shoppingbasketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBasketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
