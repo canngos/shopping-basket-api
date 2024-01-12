1c9849979b84cee131221104b59a8a68d041d490
Can Baturlar <baturlarcan@gmail.com>
2024-01-13 02:21:14 +0300

    Customer repository created

---
+++ src/main/java/com/canngos/shoppingbasketservice/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.canngos.shoppingbasketservice.repository;
+
+import com.canngos.shoppingbasketservice.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+}
