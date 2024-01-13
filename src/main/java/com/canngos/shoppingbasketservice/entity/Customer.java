cf79ef8e78f7f9a98057c4320a73c5dee20eea76
Can Baturlar <baturlarcan@gmail.com>
2024-01-13 11:12:00 +0300

    Customer columns updated

--- src/main/java/com/canngos/shoppingbasketservice/entity/Customer.java
+++ src/main/java/com/canngos/shoppingbasketservice/entity/Customer.java
@@ -1,9 +1,6 @@
 package com.canngos.shoppingbasketservice.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,10 +18,14 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String lastName;
+    @Column(unique = true, nullable = false)
     private String email;
     private String address;
+    @Column(nullable = false)
     private String password;
     @CreationTimestamp
     private LocalDateTime createdAt;
