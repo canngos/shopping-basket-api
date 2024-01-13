02c8e611e5834d2a8dc8cf186a2b8355d6d3941e
Can Baturlar <baturlarcan@gmail.com>
2024-01-13 11:12:32 +0300

    TransactionCodes added

--- src/main/java/com/canngos/shoppingbasketservice/exception/TransactionCode.java
+++ src/main/java/com/canngos/shoppingbasketservice/exception/TransactionCode.java
@@ -7,7 +7,10 @@ import org.springframework.http.HttpStatus;
 public enum TransactionCode {
     SUCCESS(100, "Success", HttpStatus.OK),
     USER_NOT_FOUND(101, "User not found", HttpStatus.NOT_FOUND),
-    INVALID_CREDENTIALS(102, "Invalid credentials", HttpStatus.BAD_REQUEST);
+    INVALID_CREDENTIALS(102, "Invalid credentials", HttpStatus.BAD_REQUEST),
+    EMAIL_EXISTS(103, "Email already exists", HttpStatus.BAD_REQUEST),
+    TOKEN_INVALID(104, "Provided token is invalid", HttpStatus.BAD_REQUEST),
+    TOKEN_EXPIRED(105, "Provided token is expired", HttpStatus.BAD_REQUEST);
 
     private final int id;
     private final String code;
