68b504b04b6ff5ef88f3a21901e578a7e1ffa3ab
Can Baturlar <baturlarcan@gmail.com>
2024-01-16 19:26:08 +0300

    Basket entity initialized

---
+++ src/main/java/com/canngos/shoppingbasketservice/entity/Basket.java
@@ -0,0 +1,30 @@
+package com.canngos.shoppingbasketservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Basket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    @OneToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    private BigDecimal totalPrice = BigDecimal.ZERO;
+    @OneToMany(mappedBy = "basket", cascade = CascadeType.ALL)
+    private List<BasketItem> basketItems = new ArrayList<>();
+}
