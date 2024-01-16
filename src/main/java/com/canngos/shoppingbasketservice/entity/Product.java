67981ddb57ba885adb7bbb7d6e971d5efa9dd71a
Can Baturlar <baturlarcan@gmail.com>
2024-01-16 23:04:49 +0300

    Product entity fields updated

--- src/main/java/com/canngos/shoppingbasketservice/entity/Product.java
+++ src/main/java/com/canngos/shoppingbasketservice/entity/Product.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Entity
 @Getter
 @Setter
@@ -15,11 +17,11 @@ public class Product {
     @Column(unique = true, nullable = false)
     private String name;
     @Column(nullable = false)
-    private Double price;
+    private BigDecimal price;
     private String description;
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private ProductType type = ProductType.OTHER;
     @Column(nullable = false)
-    private int quantity;
+    private Integer quantity;
 }
